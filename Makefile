09a7d3194c21bd728efc8240619503b0b97e273f
Averill Cate, Jr <acate@arizona.edu>
2024-05-21 16:26:48 -0700

    Testing document type choices

--- Makefile
+++ Makefile
@@ -2,29 +2,43 @@ SHELL := bash
 .ONESHELL:
 .SHELLFLAGS := -eu -o pipefail -c
 
-.PHONY: build shell chown dev_server dcu dcs fmt, bash
-
-build:
-	docker build -f Dockerfile -t catalog:latest .
-
-shell:
-	docker exec -it catalog bash
-
-chown:
-	sudo chown $(USER):$(USER) -R .
+.PHONY: build shell chown dev_server dcu dcs fmt bash migrate
 
 dcu:
 	docker compose -f compose.yml up -d --build
-	# docker run --rm -it -p 8000:8000 -v $(PWD):/catalog catalog:latest fastapi dev main.py --host 0.0.0.0 --reload
+
+dcd:
+	docker compose -f compose.yml down
 
 dcs:
 	docker compose -f compose.yml stop
 
-fmt:
-	docker run -v $(PWD):/catalog -it catalog:latest ruff format .
+shell:
+	docker exec -it catalog bash
 
-bash:
-	docker run -v $(PWD):/catalog -it catalog:latest bash
+migrate:
+	docker exec -it catalog alembic upgrade head
+# build:
+# 	docker build -f Dockerfile -t catalog:latest .
+
+# shell:
+# 	docker exec -it catalog bash
+
+# chown:
+# 	sudo chown $(USER):$(USER) -R .
+
+# dcu:
+# 	docker compose -f compose.yml up -d --build
+# 	# docker run --rm -it -p 8000:8000 -v $(PWD):/catalog catalog:latest fastapi dev main.py --host 0.0.0.0 --reload
+
+# dcs:
+# 	docker compose -f compose.yml stop
+
+# fmt:
+# 	docker run -v $(PWD):/catalog -it catalog:latest ruff format .
+
+# bash:
+# 	docker run -v $(PWD):/catalog -it catalog:latest bash
 
 # dcu:
 # 	docker compose -f compose.yml up -d --build
--- models.py
+++ models.py
@@ -6,12 +6,32 @@ from sqlalchemy.orm import declarative_base, relationship
 from typing import Optional
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.sql.expression import text
+import enum
+from sqlalchemy import Enum
+import sqlalchemy.types as types
 
 Base = declarative_base()
 target_metadata = Base.metadata
 # N_DIM = 1536
 
+# class SourceTypeEnum(enum.Enum):
+#     national_data_set = "National Data Set"
+#     data_gov = "Data.gov"
 
+class SourceType(types.TypeDecorator):
+
+    impl = types.String
+
+    def __init__(self, choices, **kw):
+        self.choices = dict(choices)
+        super(SourceType, self).__init__(**kw)
+
+    def process_bind_param(self, value, dialect):
+        return [k for k, v in self.choices.iteritems() if v == value][0]
+
+    def process_result_value(self, value, dialect):
+        return self.choices[value]
+    
 class Document(Base):
     __tablename__ = "documents"
 
@@ -19,6 +39,9 @@ class Document(Base):
     identifier = Column(String(125), nullable=True, unique=True)
     url = Column(String(250), unique=False, nullable=True)
     description = Column(String(2500), unique=False, nullable=True)
+    source_type = Column(SourceType({"national_data_set": "National Data Set", "data_dot_gov": "Data.gov"}), nullable=True)
+
+    # source_type = Column(Enum(SourceTypeEnum))
     created_at = Column(
         TIMESTAMP(timezone="TRUE"), nullable=False, server_default=text("now()")
     )
